a35ced55c6a7f41b4d537571fb92013cabab810e
Habeeb <omotee24@gmail.com>
2019-02-06 19:35:39 +0100

    Added deployment settings

--- dev_news_api/settings.py
+++ dev_news_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tjy^0lc0h7r(yiu)$_5dhbx*j)gr_2afpbq!6h@0dl&rouo_qo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['newsdev-api.herokuapp.com', 'localhost',]
 
 
 # Application definition
