be2d5366696b2b63a42b896ff267868f11466f64
Habeeb <omotee24@gmail.com>
2019-02-12 01:49:49 +0100

    Update

--- articles/tests.py
+++ articles/tests.py
@@ -26,15 +26,14 @@ class ArticleTest(APITestCase):
             bio="Renowed scientist and passionate reader."
         )
 
-        Article.objects.create(title="Old title", content="Old content", author=profile1)
-        Article.objects.create(title="Old title", content="Old content", author=profile2)
+        self.article1 = Article.objects.create(title="Old title", content="Old content", author=profile1)
+        self.article2 = Article.objects.create(title="Old title", content="Old content", author=profile2)
 
     def test_create_article(self):
         login_url = reverse('accounts-api:login')
         login_data = {'email': "jeffa@mail.com", 'password': 'Abcabc123'}
         login_response = self.client.post(login_url, login_data, format='json')
         token = login_response.data.get('token', "")
-        print('token is', token)
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
         url = reverse('articles-api:create_article')
         data = {'title': "Test 1", "content": "Content 1"}
@@ -63,3 +62,50 @@ class ArticleTest(APITestCase):
         url = reverse('articles-api:view_articles')
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_article_retrieve_by_authorized_user(self):
+        login_url = reverse('accounts-api:login')
+        login_data = {'email': "jeffa@mail.com", 'password': 'Abcabc123'}
+        login_response = self.client.post(login_url, login_data, format='json')
+        token = login_response.data.get('token', "")
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+        url = reverse('articles-api:retrieve_article', kwargs={'slug': self.article1.slug})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('title'), self.article1.title)
+
+    def test_article_retrieve_by_unauthorized_user(self):
+        url = reverse('articles-api:retrieve_article', kwargs={'slug': self.article1.slug})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_article_update_by_unauthorized_user(self):
+        url = reverse('articles-api:update_article', kwargs={'slug': self.article1.slug})
+        data = {'title': self.article2.title, 'content': 'Content has been updated'}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_article_update_by_author(self):
+        login_url = reverse('accounts-api:login')
+        login_data = {'email': "jeffa@mail.com", 'password': 'Abcabc123'}
+        login_response = self.client.post(login_url, login_data, format='json')
+        token = login_response.data.get('token', "")
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+        url = reverse('articles-api:update_article', kwargs={'slug': self.article1.slug})
+        data = {'title': self.article1.title, 'content': 'Content has been updated'}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, data)
+
+    def test_article_update_by_non_author(self):
+        login_url = reverse('accounts-api:login')
+        login_data = {'email': "jeffa@mail.com", 'password': 'Abcabc123'}
+        login_response = self.client.post(login_url, login_data, format='json')
+        token = login_response.data.get('token', "")
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+        url = reverse('articles-api:update_article', kwargs={'slug': self.article2.slug})
+        data = {'title': self.article2.title, 'content': 'Content has been updated'}
+        response = self.client.put(url, data, format='json')
+        self.assertTrue(status.is_client_error(response.status_code))
+        self.assertEqual(response.data.get('detail'), 'You are not authorized to modify this article.')
+
